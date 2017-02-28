7098012ec23e246c917c0b49a8ede133e1a98617
Justin Becker <geekman7473@gmail.com>
2017-02-28 00:54:10 -0500

    First state of FSM

--- main/main.ino
+++ main/main.ino
@@ -1,4 +1,5 @@
 #include "enes100.h"
+#include "math.h"
 
 /*
  * Replace 8 and 9 with the pins you plan on using for RX and TX 
@@ -9,35 +10,46 @@ SoftwareSerial mySerial(8,9);
 Marker marker(3); // Will have to replace # with our team's marker #
 RF_Comm rf(&mySerial, &marker);
 
-int i; 
-int val;
-const int redpin=A0;
+//Tracks state of FSM
+int state = 1; 
+//Pin for distance
+const int dSense=A0;
+
+//Drives Robot to center of staging area
+float tLocX = .5;
+float tLocY = 1;
+float tTheta = 0;
 
 void setup() {
   rf.startMission(); // Lets the Vision System (which is timing you) know that you are starting your mission
-  pinMode(redpin,OUTPUT);
+  pinMode(dSense,OUTPUT);
   Serial.begin(9600);
+
+  rf.updateLocation();
+
+  //Finds vector from starting position to center of staging area
+  tTheta = atan2(tLocX - marker.x, tLocY - marker.y);
 }
 
 void loop() {
-    distance = analogRead(redpin);  // Read the sensor
-    Serial.println(distance, DEC);   // Display value in Serial Monitor window
-    delay(250); 
-
-    // Not sure which one will work with the sensor.
-   
-    i=analogRead(redpin);
-    Serial.println(i);
-    val=(6762/(i-9))-4; // gives the distance in cm?? not sure.
-    Serial.println(val);
-
-  /* Stores the info sent in the marker object; acces by using marker.x, marker.y, and marker.theta
-   *  x and y are measured in meters from the x and y axises to the CENTER of the marker. Theta is 
-   *  measure in radians from -pi to pi (zero is parallel to X axis).
-   */
-  // rf.updateLocation(); // Boolean. Requests an update of your OSV's location & stores it in marker object.
-  
-
-  // rf.splitMission(); // Lets the Vision System know that you have finished navigation BEFORE starting Chemical objectives.
-  // rf.stopMission(); //Lets the Vision System know to stop the timer when OSV has completed ALL of its objectives.
+    rf.updateLocation(); // Boolean. Requests an update of your OSV's location & stores it in marker object.
+    
+    //Naive implementation of a FSM
+    switch (state) {
+      //State 1 turns robot to correct angle
+      case 1:
+        float err = marker.theta - tTheta;
+        if(abs(err) > 0.02){
+          turn(1); //turn right until you're facing the right direction/
+        } else {
+          state++:
+        }
+        break;
+    }
+    
 }
+
+void turn(int direction){
+  //TODO: Make this actually turn the robot...
+}
+
