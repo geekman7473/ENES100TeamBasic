cc65ba1eba82d4e4adc687d754128140c390d588
Justin T. Becker <geekman7473@gmail.com>
2017-03-05 16:13:28 -0500

    Added stuff... I think?

--- Tank_Challenge_2/Tank_Challenge_2/Tank_Challenge_2.ino
+++ Tank_Challenge_2/Tank_Challenge_2/Tank_Challenge_2.ino
@@ -2,10 +2,22 @@
 #include "enes100.h"
 #include "math.h"
 
-#define PI 3.14159264
+// Pins
+const int TRIG_PIN = 12;
+const int ECHO_PIN = 13;
+
+// Anything over 400 cm (23200 us pulse) is "out of range"
+const unsigned int MAX_DIST = 23200;
+
+// Variables for ultrasonic sensor
+unsigned long t1;
+  unsigned long t2;
+  unsigned long pulse_width;
+  float cm;
+  float inches;
 
 SoftwareSerial mySerial(8,9); 
-Marker marker(112); // Will have to replace # with our team's marker #
+Marker marker(108); // Will have to replace # with our team's marker #
 RF_Comm rf(&mySerial, &marker);
 
 
@@ -13,6 +25,12 @@ DFRTank tank;
 
 
 void setup() {
+  Serial.begin(9600);
+
+  // The Trigger pin will tell the sensor to range find
+  pinMode(TRIG_PIN, OUTPUT);
+  digitalWrite(TRIG_PIN, LOW);
+  
   rf.startMission(); // Lets the Vision System (which is timing you) know that you are starting your mission
 
   tank.init();
@@ -22,11 +40,55 @@ void setup() {
 void loop() {
   rf.updateLocation();
 
-  while(abs(marker.theta - PI/2) > 0.1){
+  //Serial.println("Distance");
+  //Serial.println(getUltrasonic());
+
+  mySerial.println("Theta");
+  mySerial.println(marker.theta);
+  mySerial.println(marker.theta - (3.14159653/2.0));
+  mySerial.println(abs(marker.theta - (3.14159653/2.0)));
+  
+  while(abs(marker.theta - (3.14159653/2.0)) > 0.01){
     tank.setLeftMotorPWM(-175);
     tank.setRightMotorPWM(175);
     rf.updateLocation();
   }
-  
-  tank.turnOffMotors();
+    
+  //tank.turnOffMotors();
 }
+
+float getUltrasonic(){
+  
+  unsigned long t1;
+  unsigned long t2;
+  unsigned long pulse_width;
+  float cm;
+  float inches;
+
+  // Hold the trigger pin high for at least 10 us
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  // Wait for pulse on echo pin
+  while ( digitalRead(ECHO_PIN) == 0 );
+
+  // Measure how long the echo pin was held high (pulse width)
+  // Note: the micros() counter will overflow after ~70 min
+  t1 = micros();
+  while ( digitalRead(ECHO_PIN) == 1);
+  t2 = micros();
+  pulse_width = t2 - t1;
+
+  // Calculate distance in centimeters and inches. The constants
+  // are found in the datasheet, and calculated from the assumed speed 
+  //of sound in air at sea level (~340 m/s).
+  cm = pulse_width / 58.0;
+  inches = pulse_width / 148.0;
+
+  // Wait at least 60ms before next measurement
+  delay(60);
+
+  return cm;
+}
+
